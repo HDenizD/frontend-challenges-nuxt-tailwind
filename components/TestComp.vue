9b09a3af26618cc972f29d4c5326f959f9659b42
Deniz Derezzed <denizderezzed@gmail.com>
2023-11-20 08:56:25 +0100

    wip dark mode

---
+++ components/TestComp.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>just a test component</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    :class="{ dark: isDark }"
+    class="bg-white dark:bg-dark text-white dark:text-black p-3 h-screen"
+  >
+    <button
+      class="border-solid border-2 border-sky-500 rounded-md p-2"
+      @click="toggleDarkMode"
+    >
+      Toggle Dark Mode
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const isDark = ref(false)
+
+const toggleDarkMode = () => {
+  console.log(isDark.value)
+  isDark.value = !isDark.value
+}
+</script>
+
+<style scoped></style>
