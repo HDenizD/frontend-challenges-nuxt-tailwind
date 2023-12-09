e88623d1696e03292ec3f8618842dd6dde60ab54
Deniz Derezzed <denizderezzed@gmail.com>
2023-12-09 13:53:59 +0100

    asd

--- components/multiStepForm/SelectPlan.vue
+++ components/multiStepForm/SelectPlan.vue
@@ -26,6 +26,24 @@
         @click="selectPlan('pro')"
       />
     </div>
+    <div
+      class="h-10 bg-blue-50 rounded-md mt-3 gap-5 flex items-center justify-center"
+    >
+      <div class="font-bold text-blue-950">Monthly</div>
+      <div>
+        <label class="relative items-center cursor-pointer">
+          <input
+            type="checkbox"
+            value=""
+            class="sr-only peer"
+          />
+          <div
+            class="w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600"
+          ></div>
+        </label>
+      </div>
+      <div class="font-bold text-blue-950">Yearly</div>
+    </div>
   </MultiStepFormDefaultStep>
 </template>
 
