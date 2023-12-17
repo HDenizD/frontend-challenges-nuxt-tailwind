da4971a74fd05e54ed973eac9e2da883f1c22442
Deniz Derezzed <denizderezzed@gmail.com>
2023-12-17 10:00:52 +0100

    Refactor multiStepForm component styling

--- components/multiStepForm/MultiStepForm.vue
+++ components/multiStepForm/MultiStepForm.vue
@@ -4,10 +4,13 @@
   >
     <MultiStepFormStepper :step-index="multiStepFormStore.stepIndex" />
     <div
-      class="inline-flex flex-col md:mx-20 w-full justify-between bg-[#ebf4fb] md:bg-white md:h-auto"
+      class="flex flex-col md:mx-20 w-full justify-between bg-[#ebf4fb] md:bg-white md:h-auto"
     >
       <!-- Stepper Content -->
-      <div class="bg-white p-8 my-5 mx-0 -top-20 relative md:static rounded-xl">
+      <div
+        v-if="!orderConfirmed"
+        class="bg-white p-8 my-5 md:mx-0 mx-5 -top-20 relative md:static rounded-xl"
+      >
         <MultiStepFormPersonalInfo v-if="multiStepFormStore.stepIndex === 0" />
         <MultiStepFormSelectPlan v-if="multiStepFormStore.stepIndex === 1" />
         <MultiStepFormPickAddons v-if="multiStepFormStore.stepIndex === 2" />
@@ -16,27 +19,29 @@
         />
         <!-- Stepper Content -->
       </div>
+      <MultiStepFormPostConfirm v-else />
       <div
-        class="flex fixed md:static w-full bottom-0 md:py-0 justify-between p-4 mb-3 bg-white md:shadow-none shadow-md shadow-black"
+        class="flex fixed md:static w-full bottom-0 md:py-0 justify-between p-4 md:mb-3 shadow-black"
+        :class="orderConfirmed ? 'bg-[#ebf4fb]' : 'bg-white'"
       >
         <button
-          v-if="multiStepFormStore.stepIndex !== 0"
+          v-if="multiStepFormStore.stepIndex !== 0 && !orderConfirmed"
           class="text-blue-900 w-32 select-none hover:text-blue-700 font-bold py-3 px-5 rounded-lg"
           @click="multiStepFormStore.cycleStepIndex('backward')"
         >
           Go Back
         </button>
         <button
-          v-if="multiStepFormStore.stepIndex !== 3"
+          v-if="multiStepFormStore.stepIndex !== 3 && !orderConfirmed"
           class="bg-blue-900 w-32 select-none hover:bg-blue-700 text-white py-3 px-5 rounded-lg ml-auto"
           @click="multiStepFormStore.cycleStepIndex('forward')"
         >
           Next Step
         </button>
         <button
-          v-if="multiStepFormStore.stepIndex === 3"
+          v-if="multiStepFormStore.stepIndex === 3 && !orderConfirmed"
           class="bg-indigo-600 w-32 select-none hover:bg-blue-700 text-white py-3 px-5 rounded-lg ml-auto"
-          @click="multiStepFormStore.cycleStepIndex('forward')"
+          @click="orderConfirmed = true"
         >
           Confirm
         </button>
@@ -47,6 +52,7 @@
 
 <script setup lang="ts">
 import { useMultiStepForm } from '@/store/multiStepForm'
+import type PostConfirmVue from './PostConfirm.vue'
 const multiStepFormStore = useMultiStepForm()
 
 function detectTailwindMobileAndChangeBgColor() {
@@ -58,6 +64,8 @@ function detectTailwindMobileAndChangeBgColor() {
     }
   }
 }
+
+const orderConfirmed = ref(false)
 
 onMounted(() => {
   detectTailwindMobileAndChangeBgColor()
---
+++ components/multiStepForm/PostConfirm.vue
@@ -0,0 +1,38 @@
+<template>
+  <MultiStepFormDefaultStep
+    title=""
+    sub-title=""
+  >
+    <div
+      class="flex justify-center items-center py-20 bg-white p-8 my-5 md:mx-0 mx-5 -top-28 relative md:static rounded-xl"
+    >
+      <div class="flex flex-col">
+        <img
+          :src="thankYouIcon"
+          alt=""
+          class="self-center mb-5"
+        />
+        <div class="text-3xl text-blue-950 font-bold text-center mb-4">
+          Thank you!
+        </div>
+        <p class="text-center text-gray-400 mx-5">
+          Thanks for confirming your subscription! We hope you have fun using
+          our platform. If you ever need support, please feel free to email us
+          at
+          <a
+            href="mailto:support@loremgaming.com"
+            class="underline text-blue-800"
+          >
+            support@loremgaming.com</a
+          >.
+        </p>
+      </div>
+    </div>
+  </MultiStepFormDefaultStep>
+</template>
+
+<script setup lang="ts">
+import thankYouIcon from '@/challenges/multi-step-form/assets/images/icon-thank-you.svg'
+</script>
+
+<style scoped></style>
