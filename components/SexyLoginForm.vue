09099c069849f78eb8cd9e98fbefe5bfcc16156c
Deniz Derezzed <denizderezzed@gmail.com>
2023-12-28 15:11:15 +0100

    asd

--- components/SexyLoginForm.vue
+++ components/SexyLoginForm.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="flex justify-center items-center overflow-hidden m-12">
+  <div class="p-1 flex justify-center items-center overflow-hidden m-12">
     <form
       class="w-[500px] h-[800px] rounded-2xl relative flex justify-center items-center bg-[#121212]"
     >
-      <div class="w-[510px] h-[950px] absolute rounded-2xl overflow-hidden">
+      <div class="w-[102%] h-[150%] absolute overflow-hidden">
         <div class="bg-gradient-cone w-full h-full"></div>
       </div>
-      <div class="z-10 absolute w-full h-full rounded-2xl">
+      <div class="z-10 bg-[#121212] absolute w-full h-full">
         <MultiStepFormBaseInput
           v-model="email"
           class="text-dark p-5"
@@ -41,8 +41,8 @@ const email = ref('')
     rgba(99, 0, 212, 0) 88%,
     rgba(99, 0, 212, 1) 100%
   );
-  background-size: contain;
-  background-position: center;
+  /* background-size: contain;
+  background-position: center; */
 
   animation: rotate 5s infinite linear;
 }
